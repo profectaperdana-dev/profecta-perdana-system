30082868d0956795f254c7e73e206217f4c7cc6a
alfarisy-tech <ariafsundoro@gmail.com>
2022-08-03 12:10:34 +0700

    FORM CREATE PRODUCTS

--- resources/views/products/_form.blade.php
+++ resources/views/products/_form.blade.php
@@ -52,17 +52,48 @@
     </div>
     <div class="col-md-12">
         <div class="form-group row font-weight-bold">
-            <div class="col-md-4">
-                <label>Product Code</label>
-                <input type="text" class="form-control" placeholder="Product Code">
+            <div class="col-md-3">
+                <label>Product Weight</label>
+                <input type="number" class="form-control" placeholder="Product Weight">
             </div>
-            <div class="col-md-4">
-                <label>Product Name</label>
-                <input type="text" class="form-control" placeholder="Product Name">
+            <div class="col-md-3">
+                <label>Purchase Price</label>
+                <input type="number" class="form-control" placeholder="Purchase Price">
             </div>
-            <div class="col-md-4">
-                <label>Serial Number</label>
-                <input type="text" class="form-control" placeholder="Serial Number">
+            <div class="col-md-3">
+                <label>Retail Selling Price </label>
+                <input type="number" class="form-control" placeholder="Retail Selling Price">
+            </div>
+            <div class="col-md-3">
+                <label>Non Retail Selling Price</label>
+                <input type="number" class="form-control" placeholder="Non Retail Selling Price">
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="form-group row font-weight-bold">
+            <div class="col-md-3">
+                <label>Qty Stock</label>
+                <input type="number" class="form-control" placeholder="Qty Stock">
+            </div>
+            <div class="col-md-3">
+                <label>Min Stock</label>
+                <input type="number" class="form-control" placeholder="Min Stock">
+            </div>
+            <div class="col-md-3">
+                <label>Status</label>
+                <input type="number" class="form-control" placeholder="Status">
+            </div>
+            <div class="col-md-3">
+                <label>Product Photo</label>
+                <input type="file" class="form-control" placeholder="Product Photo">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-12">
+                <a href="{{ URL::previous() }}" class="btn btn-danger">Back</a>
+                <button type="reset" class="btn btn-warning" data-dismiss="modal">Reset</button>
+                <button type="submit" class="btn btn-primary">Save</button>
             </div>
         </div>
     </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -16,7 +16,6 @@
 
 
         <div class="row">
-
             <div class="col-md-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
