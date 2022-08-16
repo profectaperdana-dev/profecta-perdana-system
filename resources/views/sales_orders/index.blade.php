70de700e6b952c4373fea4350fb79dc644d9ec6f
Arizli Romadhon <arielromadhon7@gmail.com>
2022-08-16 08:32:19 +0700

    finishing dynamic form

--- resources/views/sales_orders/index.blade.php
+++ resources/views/sales_orders/index.blade.php
@@ -1,184 +1,159 @@
 @extends('layouts.master')
 @section('content')
-    @push('css')
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/datatables.css') }}">
-    @endpush
+  @push('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/datatables.css') }}">
+  @endpush
 
-    <div class="container-fluid">
-        <div class="page-header">
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3 class="font-weight-bold"> {{ $title }}</h3>
-                    <h6 class="font-weight-normal mb-0 breadcrumb-item active">Create, Read, Update and Delete
-                        {{ $title }}
-                </div>
-
-            </div>
+  <div class="container-fluid">
+    <div class="page-header">
+      <div class="row">
+        <div class="col-sm-6">
+          <h3 class="font-weight-bold"> {{ $title }}</h3>
+          <h6 class="font-weight-normal mb-0 breadcrumb-item active">Create, Read, Update and Delete
+            {{ $title }}
         </div>
-    </div>
-    <!-- Container-fluid starts-->
-    <div class="container-fluid">
-        <div class="row">
 
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header pb-0">
-                        <h5>Create Data</h5>
-                        <hr class="bg-primary">
-                    </div>
-                    <div class="card-body">
-                        <form method="post" action="{{ url('sales_order/') }}" enctype="multipart/form-data"
-                            id="">
-                            @csrf
-                            @include('sales_orders._form')
-                        </form>
-                    </div>
-                </div>
-            </div>
+      </div>
+    </div>
+  </div>
+  <!-- Container-fluid starts-->
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-sm-12">
+        <div class="card">
+          <div class="card-header pb-0">
+            <h5>Create Data</h5>
+            <hr class="bg-primary">
+          </div>
+          <div class="card-body">
+            <form method="post" action="{{ url('sales_order/') }}" enctype="multipart/form-data" id="">
+              @csrf
+              @include('sales_orders._form')
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-    <!-- Container-fluid Ends-->
-    @push('scripts')
-        <script src="{{ asset('assets/js/datatable/datatables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('assets/js/datatable/datatables/datatable.custom.js') }}"></script>
-        <script src="{{ asset('js/custom.js') }}"></script>
-        <script>
-            let csrf = $('meta[name="csrf-token"]').attr("content");
+  </div>
+  <!-- Container-fluid Ends-->
+  @push('scripts')
+    <script src="{{ asset('assets/js/datatable/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/datatable/datatables/datatable.custom.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+    <script>
+      let csrf = $('meta[name="csrf-token"]').attr("content");
 
-            $(".productSo").select2({
-                width: "100%",
-                ajax: {
-                    type: "GET",
-                    url: "{{ url('/products/select') }}",
-                    data: {
-                        _token: csrf,
-                    },
-                    dataType: "json",
-                    delay: 250,
-                    processResults: function(data) {
-                        return {
-                            results: $.map(data, function(item) {
-                                return [{
-                                    text: item.nama_barang,
-                                    id: item.id,
-                                }, ];
-                            }),
-                        };
-                    },
-                },
-            });
+      $(".productSo").select2({
+        width: "100%",
+        ajax: {
+          type: "GET",
+          url: "{{ url('/products/select') }}",
+          data: {
+            _token: csrf,
+          },
+          dataType: "json",
+          delay: 250,
+          processResults: function(data) {
+            return {
+              results: $.map(data, function(item) {
+                return [{
+                  text: item.nama_barang,
+                  id: item.id,
+                }, ];
+              }),
+            };
+          },
+        },
+      });
 
-            //Get Customer ID
-            let customer_id = "";
-            $(".customer-append").change(function() {
-                customer_id = $(".customer-append").val();
-            });
+      //Get Customer ID
+      let customer_id = "";
+      $(".customer-append").change(function() {
+        customer_id = $(".customer-append").val();
+      });
 
-            let list_product = [];
-            let x = 0;
-            let product_id = 0;
-            //Get discount depent on product
-            $(document).on("change", ".productSo", function() {
-                product_id = $(this).val();
+      let x = 0;
+      let product_id = 0;
+      //Get discount depent on product
+      $(document).on("change", ".productSo", function() {
+        product_id = $(this).val();
 
-                let parent_product = $(this)
-                    .parent()
-                    .siblings()
-                    .find(".discount-append");
+        let parent_product = $(this)
+          .parent()
+          .siblings()
+          .find(".discount-append");
 
-                $.ajax({
-                    type: "GET",
-                    url: "{{ url('/discounts/select') }}" + "/" + customer_id + "/" + product_id,
-                    dataType: "json",
-                    success: function(data) {
-                        parent_product.val(data.discount);
-                    },
-                });
-            });
+        $.ajax({
+          type: "GET",
+          url: "{{ url('/discounts/select') }}" + "/" + customer_id + "/" + product_id,
+          dataType: "json",
+          success: function(data) {
+            parent_product.val(data.discount);
+          },
+        });
+      });
 
-            $("#addSo").on("click", function() {
-                ++x;
-                list_product.push(product_id);
-                let form =
-                    '<div class="form-group row">' +
-                    '<div class="form-group col-4">' +
-                    "<label>Product</label>" +
-                    '<select name="soFields[' +
-                    x +
-                    '][product_id]" class="form-control productSo2" required>' +
-                    '<option value=""> Choose Product </option> ' +
-                    "</select>" +
-                    "</div>" +
-                    '<div class="col-3 col-md-3 form-group">' +
-                    "<label> Qty </label> " +
-                    '<input class="form-control" name="soFields[' +
-                    x +
-                    '][qty]">' +
-                    "</div> " +
-                    '<div class="col-3 col-md-4 form-group">' +
-                    "<label>Discount %</label>" +
-                    '<input class="form-control discount-append" name="soFields[0][discount]" id="">' +
-                    "</div>" +
-                    '<div class="col-2 col-md-1 form-group">' +
-                    '<label for=""> &nbsp; </label>' +
-                    '<a class="form-control text-white remSo text-center" style="border:none; background-color:red">' +
-                    "- </a> " +
-                    "</div>" +
-                    " </div>";
-                $("#formSo").append(form);
+      $("#addSo").on("click", function() {
+        ++x;
+        let form =
+          '<div class="form-group row">' +
+          '<div class="form-group col-4">' +
+          "<label>Product</label>" +
+          '<select name="soFields[' +
+          x +
+          '][product_id]" class="form-control productSo" required>' +
+          '<option value=""> Choose Product </option> ' +
+          "</select>" +
+          "</div>" +
+          '<div class="col-3 col-md-3 form-group">' +
+          "<label> Qty </label> " +
+          '<input class="form-control" name="soFields[' +
+          x +
+          '][qty]">' +
+          "</div> " +
+          '<div class="col-3 col-md-4 form-group">' +
+          "<label>Discount %</label>" +
+          '<input class="form-control discount-append" name="soFields[0][discount]" id="" readonly>' +
+          "</div>" +
+          '<div class="col-2 col-md-1 form-group">' +
+          '<label for=""> &nbsp; </label>' +
+          '<a class="form-control text-white remSo text-center" style="border:none; background-color:red">' +
+          "- </a> " +
+          "</div>" +
+          " </div>";
+        $("#formSo").append(form);
 
-                //let set_product = [...new Set(list_product)];
-                let set_product = [...new Set(list_product)];
-                //select2 product
-                $(".productSo2").select2({
-                    width: "100%",
-                    ajax: {
-                        type: "GET",
-                        url: "{{ url('/products/select_without') }}",
-                        data: {
-                            _token: csrf,
-                            data: set_product
-                        },
-                        dataType: "json",
-                        delay: 250,
-                        processResults: function(data) {
-                            return {
-                                results: $.map(data, function(item) {
-                                    return [{
-                                        text: item.nama_barang,
-                                        id: item.id,
-                                    }, ];
-                                }),
-                            };
-                        },
-                    },
-                });
-
-                $(document).on("change", ".productSo2", function() {
-                    product_id = $(this).val();
-
-                    let parent_product = $(this)
-                        .parent()
-                        .siblings()
-                        .find(".discount-append");
-
-                    $.ajax({
-                        type: "GET",
-                        url: "{{ url('/discounts/select') }}" + "/" + customer_id + "/" + product_id,
-                        dataType: "json",
-                        success: function(data) {
-                            parent_product.val(data.discount);
-                        },
-                    });
-                });
-            });
+        $(".productSo").select2({
+          width: "100%",
+          ajax: {
+            type: "GET",
+            url: "{{ url('/products/select') }}",
+            data: {
+              _token: csrf,
+            },
+            dataType: "json",
+            delay: 250,
+            processResults: function(data) {
+              return {
+                results: $.map(data, function(item) {
+                  return [{
+                    text: item.nama_barang,
+                    id: item.id,
+                  }, ];
+                }),
+              };
+            },
+          },
+        });
 
 
+      });
 
-            //remove Sales Order fields
-            $(document).on("click", ".remSo", function() {
-                $(this).parents(".form-group").remove();
-            });
-        </script>
-    @endpush
+      //remove Sales Order fields
+      $(document).on("click", ".remSo", function() {
+        $(this).parents(".form-group").remove();
+      });
+    </script>
+  @endpush
 @endsection
