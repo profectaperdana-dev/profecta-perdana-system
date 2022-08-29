355fcfdef11690c02c9bfee309f4b925448196bb
Arizli Romadhon <arielromadhon7@gmail.com>
2022-08-29 09:34:43 +0700

    fixing sound

--- resources/views/layouts/partials/notify.blade.php
+++ resources/views/layouts/partials/notify.blade.php
@@ -1,68 +1,68 @@
 @can('isVerificator')
-    <script>
-        Echo.channel('realtimeNotif').listen('SOMessage', (e) => {
-            let sound = new Audio("{{ asset('sounds/so-created.wav') }}");
-            sound.play();
-            $.notify({
-                title: e.title,
-                message: e.message
-            }, {
-                type: 'primary',
-                allow_dismiss: true,
-                newest_on_top: true,
-                mouse_over: true,
-                showProgressbar: false,
-                spacing: 10,
-                timer: 10000,
-                placement: {
-                    from: 'top',
-                    align: 'right'
-                },
-                offset: {
-                    x: 30,
-                    y: 30
-                },
-                delay: 1000,
-                z_index: 10000,
-                animate: {
-                    enter: 'animated swing',
-                    exit: 'animated swing'
-                }
-            })
-        });
-    </script>
+  <script>
+    Echo.channel('realtimeNotif').listen('SOMessage', (e) => {
+      let sound = new Audio("{{ asset('sounds/so-created.wav') }}");
+      sound.play();
+      $.notify({
+        title: e.title,
+        message: e.message
+      }, {
+        type: 'primary',
+        allow_dismiss: true,
+        newest_on_top: true,
+        mouse_over: true,
+        showProgressbar: false,
+        spacing: 10,
+        timer: 10000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        },
+        offset: {
+          x: 30,
+          y: 30
+        },
+        delay: 1000,
+        z_index: 10000,
+        animate: {
+          enter: 'animated swing',
+          exit: 'animated swing'
+        }
+      })
+    });
+  </script>
 @endcan
 @can('isAdmin')
-    <script>
-        Echo.channel('approval_notify').listen('ApprovalMessage', (e) => {
-            let sound = new Audio("{{ asset('sounds/so-created.wav') }}");
-            sound.play();
-            $.notify({
-                title: e.title,
-                message: e.message
-            }, {
-                type: 'danger',
-                allow_dismiss: true,
-                newest_on_top: true,
-                mouse_over: true,
-                showProgressbar: false,
-                spacing: 10,
-                timer: 10000,
-                placement: {
-                    from: 'top',
-                    align: 'right'
-                },
-                offset: {
-                    x: 30,
-                    y: 30
-                },
-                delay: 1000,
-                z_index: 10000,
-                animate: {
-                    enter: 'animated swing',
-                    exit: 'animated swing'
-                }
-            })
-        });
-    </script>
+  <script>
+    Echo.channel('approval_notif').listen('ApprovalMessage', (e) => {
+      let sound = new Audio("{{ asset('sounds/so-created.wav') }}");
+      sound.play();
+      $.notify({
+        title: e.title,
+        message: e.message
+      }, {
+        type: 'danger',
+        allow_dismiss: true,
+        newest_on_top: true,
+        mouse_over: true,
+        showProgressbar: false,
+        spacing: 10,
+        timer: 10000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        },
+        offset: {
+          x: 30,
+          y: 30
+        },
+        delay: 1000,
+        z_index: 10000,
+        animate: {
+          enter: 'animated swing',
+          exit: 'animated swing'
+        }
+      })
+    });
+  </script>
 @endcan
